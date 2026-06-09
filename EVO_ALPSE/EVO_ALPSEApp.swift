61c949377457dfdd43bd1270387f14df80c46c90
caarat156 <anastasiaeugenem@gmail.com>
2026-06-09 14:01:01 +0700

    Initial Commit

---
+++ EVO_ALPSE/EVO_ALPSEApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVO_ALPSEApp.swift
+//  EVO_ALPSE
+//
+//  Created by Anastasia on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct EVO_ALPSEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
