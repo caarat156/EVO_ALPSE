4b7bfb352c27a0a7a7baa8c2f9b90b3a96956027
Owennn04 <owentengkawan@gmail.com>
2026-06-10 22:31:24 +0700

    Vendor view

---
+++ EVO_ALPSE/View/VendorView.swift
@@ -0,0 +1,18 @@
+//
+//  VendorView.swift
+//  EVO_ALPSE
+//
+//  Created by Angelique Kyra on 10/06/26.
+//
+
+import SwiftUI
+
+struct VendorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VendorView()
+}
