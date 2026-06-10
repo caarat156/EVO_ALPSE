bd832bf1671f5460fadff14bdfc8360afa595b37
Angelique Kyra <angeliquekw50@gmail.com>
2026-06-10 21:53:40 +0700

    peserta views and feedback model

---
+++ EVO_ALPSE/View/PesertaViews.swift
@@ -0,0 +1,18 @@
+//
+//  PesertaViews.swift
+//  EVO_ALPSE
+//
+//  Created by Angelique Kyra on 10/06/26.
+//
+
+import SwiftUI
+
+struct PesertaViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PesertaViews()
+}
